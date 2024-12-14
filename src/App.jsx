a65bb128bc96b1affb5ed9b2949325777c4751f3
sharifmasyhur <sharifalhadyyad@gmail.com>
2024-12-14 19:28:08 +0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Background from "./components/background/Background"
 import Navbar from "./components/navbar/Navbar"
 import Hero from "./components/hero/Hero"
--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,8 +1,8 @@
 import './Background.css'
-import video1 from '../../assets/video1.mp4'
-import image1 from '../../assets/image1.jpg'
-import image2 from '../../assets/image2.png'
-import image3 from '../../assets/image3.png'
+import video1 from '../../Assets/video1.mp4'
+import image1 from '../../Assets/image1.jpg'
+import image2 from '../../Assets/image2.png'
+import image3 from '../../Assets/image3.png'
 
 export const Background = ({playStatus,heroCount}) => {
   
